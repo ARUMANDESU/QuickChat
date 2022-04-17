cdefcc9e2d2ff74135abb8eabd4e4dd6458e8143
Arumandesu <zh.arumandes@gmail.com>
2022-04-17 21:28:45 +0600

    ejs +  Giphy api (in chat.ejs)

--- server.js
+++ server.js
@@ -1,12 +1,15 @@
 const express = require('express');
+const https =require("https");
 const bodyParser = require("body-parser");
-const app = express()
+const path  = require('path');
+const app = express();
+const port = 3000;
 
-const port = 3000
+app.set('views', path.join(__dirname,'views'))
+app.set('view engine', 'ejs')
 
-//using images from "images" folder
+
 app.use("/public",express.static(__dirname + '/public'));
-
 
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -15,20 +18,46 @@ app.get('/', (req, res) =>{
     res.redirect("/home")
 })
 app.get("/home",(req,res)=>{
-    res.sendFile(__dirname+"/index.html")
+    res.render("index")
 })
 app.get("/about",(req,res)=>{
-    res.sendFile(__dirname+"/about.html")
+    res.render("about")
 })
 app.get("/chat",(req,res)=>{
-    res.sendFile(__dirname+"/chat.html")
+    res.render("chat")
 })
 
 
+app.post("/chat",(req,res)=>{
+    const api_key='FjcUNrdCq0IX06NM4o3M8iWJoVs2FZhC' //get from giphy developers
+    const name = req.body.gifname;
+
+    const url='https://api.giphy.com/v1/gifs/search?api_key='+api_key+'&limit=1&q='+name;
+    
+    https.get(url,(response)=>{
+        console.log(response.statusCode);
+
+        //
+        response.setEncoding('utf8');
+        let body = '';
+        response.on('data', (d) => {
+            
+             body += d;
+        });
+       
+        response.on('end', () => {
+            let parsed = JSON.parse(body);
+            let imgUrl =parsed.data[0].images.downsized.url
+            
+            res.render('chat',{gifImg:imgUrl})
+
+        })
+    })
+})
 
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
 
 app.use((req,res)=>{
-    res.sendFile(__dirname+"/error.html")
+    res.render("error")
 })
