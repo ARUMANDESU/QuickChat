16f855a5a566a18181cda15b8ac02c772853a109
Arumandesu <zh.arumandes@gmail.com>
2022-04-19 21:46:54 +0600

    unsplash api

--- server.js
+++ server.js
@@ -21,9 +21,28 @@ app.get("/home",(req,res)=>{
     res.render("index")
 })
 app.get("/about",(req,res)=>{
-    res.render("about")
+    // unsplash api
+    const api_key='9ikFXSSzfgU68HWrQ5PF_08eQNUUzwi-KduO5MLDv1M' //get from unsplash developers
+    const url=`https://api.unsplash.com/photos/random/?client_id=${api_key}`;
+    https.get(url,(response)=>{
+        console.log(response.statusCode);
+        response.setEncoding('utf8');
+        // without this part of code response don't give us whole json file (found this solution from internet)
+        let body = '';
+        response.on('data', (d) => {
+             body += d;
+        });
+       
+        response.on('end', () => {
+            let parsedImg = JSON.parse(body);
+            
+            res.render('about',{randomImg:parsedImg.urls.raw})
+
+        })
+    })
 })
 app.get("/chat",(req,res)=>{
+
     res.render("chat",{parsed:[]})
 })
 
