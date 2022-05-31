9c862be4b0560da044b17a2f668b8e077635d6c7
Arumandesu <zh.arumandes@gmail.com>
2022-05-31 22:04:32 +0600

    random chat

--- public/js/index.js
+++ public/js/index.js
@@ -1,5 +1,5 @@
 function randomUser(){
-     fetch('api/getRandomUser',{
+     fetch('/api/getRandomUser',{
         method:'GET',
         headers:{'Content-Type':'application/json'},
     }).then(data=>data.json()).then(data=>{location.href=`/chat/${data.user.username}`})
